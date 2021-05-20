3482866b7cfac8cc70e294fccb324a9bc7c97879
dhanasekhar-podigiri-by <82364685+dhanasekhar-podigiri-by@users.noreply.github.com>
2021-05-20 19:50:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Node App Deployment\n');
+  res.send('Node App Deployment stratosphere\n');
 });
 
 app.listen(PORT, HOST);
